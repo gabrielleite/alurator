4adae91ddd35c7ad2e37aa9eda6fe7bf946a46d4
Gabriel Leite <gabrielcfcleite@gmail.com>
2018-04-27 03:09:44 -0300

    Mais uma correção no estado inicial.

--- src/br/com/alura/alurator/Alurator.java
+++ src/br/com/alura/alurator/Alurator.java
@@ -1,18 +1,8 @@
 package br.com.alura.alurator;
 
-import br.com.alura.alurator.protocolo.Request;
-
 public class Alurator {
 	
-	private String basePackage;
-
-	public Alurator(String basePackage) {
-		this.basePackage = basePackage;
-	}
-	
 	public Object executa(String url) {
-		Request request = new Request(url);
-		
 		// TODO - processa a requisicao executando o metodo
 		// da classe em questao
 		
